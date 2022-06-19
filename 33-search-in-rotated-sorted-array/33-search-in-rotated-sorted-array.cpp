03d10879f059576b5631eb158a9fa5297eb32cbb
RakhiChaudhary <93258616+RakhiChaudhary@users.noreply.github.com>
2022-06-20 04:15:26 +0530

    Time: 4 ms (69.58%), Space: 11 MB (74.88%) - LeetHub

---
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -0,0 +1,97 @@
+class Solution {
+public:
+    /*int bSearch(vector<int> &nums , int low , int high , int target){
+        
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            if(nums[mid] == target)
+                return mid;
+            else if(nums[mid] > target)
+                high = mid - 1;
+            else low = mid + 1;
+        }
+        return -1;
+    }
+    
+    int solve(vector<int>&nums,int start,int end){
+        end=nums.size();
+        int mid;
+        int n=nums.size();
+        int next=(mid+1)%n;
+        int prev=(mid-1)%n;
+        while(start<=end){
+            mid=start+(end-start)/2;
+            if(nums[mid]<=nums[next] && nums[mid]<=nums[prev])
+                return mid;
+            if(nums[start]<=nums[mid])
+                start=mid+1;
+            else  if(nums[mid])
+                end=mid-1;
+        }
+        return -1;
+    }*/
+    int search(vector<int>& nums, int target) {
+      /*  int start=0;
+        int end=nums.size();
+        int ans=0;
+        int index=solve(nums,0,end);
+        if(index==-1)
+            return index;
+        else{
+        int index1=bSearch(nums,0,index-1,target);
+        int index2=bSearch(nums,index,end,target);
+              ans=max(index1,index2);  
+        }
+        
+        return ans;*/
+        
+     /* int start=0;
+        int end=nums.size()-1;
+        
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(nums[mid]==target)
+                return mid;
+            
+            if(nums[start]<=nums[mid]){
+                if(target >= nums[start] && target <= [mid])
+                    end=mid-1;
+                    else
+                    start=mid+1;
+            }
+            
+            else{
+                if(target>=nums[mid] && target<=nums[end])
+                {
+                    start=mid+1;
+                }
+                else
+                    end=mid-1;
+            }
+        }
+        return -1;*/
+        
+                
+        
+  int low = 0, high = nums.size() - 1; //<---step 1
+
+  while (low <= high) { //<--- step 2
+    int mid = (low + high) >> 1; //<----step 3
+    if (nums[mid] == target)
+      return mid; // <---step 4
+
+    if (nums[low] <= nums[mid]) { //<---step 5
+      if (nums[low] <= target && nums[mid] >= target)
+        high = mid - 1; //<---step 6 
+      else
+        low = mid + 1; //<---step 7
+    } else { //<---step 7
+      if (nums[mid] <= target && target <= nums[high])
+        low = mid + 1;
+      else
+        high = mid - 1;
+    }
+  }
+  return -1; //<--
+    }
+};
