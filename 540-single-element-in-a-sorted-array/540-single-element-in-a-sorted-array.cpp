4f251e29c3b69d319c052bc371d5f49a10b35682
RakhiChaudhary <93258616+RakhiChaudhary@users.noreply.github.com>
2022-06-20 05:44:49 +0530

    Time: 23 ms (96.65%), Space: 22.4 MB (8.79%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
@@ -39,8 +39,7 @@ public:
         int end=nums.size()-1;
        
          int mid;
-        int prev=(mid-1)%n;
-        int next=(mid+1)%n;
+       
         if(n == 1) return nums[0];
         else if(nums[0] != nums[1]) return nums[0];
         else if(nums[n-1] != nums[n-2]) return nums[n-1];
@@ -49,9 +48,13 @@ public:
           mid=start+(end-start)/2;
             //if((nums[mid]!=nums[prev]) && (nums[mid]!=nums[next]))
                // return mid;
-            if((mid%2==0 && nums[mid]==nums[mid+1]) ||(mid%2==1 && nums[mid]==nums[mid-1] ))
+             if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1]) return nums[mid];
+             
+            if(mid%2 == 0 && nums[mid+1] == nums[mid] || 
+               mid%2 == 1 && nums[mid-1] == nums[mid])
+            
             {
-        
+                   
                     start=mid+1;
                
             }
