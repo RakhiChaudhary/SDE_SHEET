8866b3927a26f6e3159d97df146171ff757fe9c1
RakhiChaudhary <93258616+RakhiChaudhary@users.noreply.github.com>
2022-08-17 12:34:41 +0530

    Time: 15 ms (5.07%), Space: 8.6 MB (61.19%) - LeetHub

--- 804-unique-morse-code-words/804-unique-morse-code-words.cpp
+++ 804-unique-morse-code-words/804-unique-morse-code-words.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
     int uniqueMorseRepresentations(vector<string>& words) {
-        string a[26]={".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
         
-        int n=words.size();
+         int n=words.size();
         
         unordered_map<string,int>mp;
+        
+        string arr[26]={".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        
+       
         for(int i=0;i<n;i++){
             string s="";
             int m=words[i].size();
             for(int j=0;j<m;j++){
-                s=s+a[words[i][j]-'a'];
+                s=s+arr[words[i][j]-'a'];
                 
             }
             mp[s]=1;
         }
         return mp.size();
+        
     }
 };
