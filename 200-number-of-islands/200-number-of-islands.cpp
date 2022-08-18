03b867fee0bab0c5847bde84baec83b5ba3a50a2
RakhiChaudhary <93258616+RakhiChaudhary@users.noreply.github.com>
2022-08-18 11:04:05 +0530

    Time: 47 ms (68.97%), Space: 19.2 MB (8.85%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,40 +1,93 @@
 class Solution {
 public:
-        void mark_curr_island(vector<vector<char>> &grid, int x,int y, int r, int c){
-
-        if(x<0 || x>=r || y<0 || y>=c || grid[x][y] != '1')
-            return;
-
-        grid[x][y] = '2';
-
-        mark_curr_island(grid,x+1,y,r,c); // Makes Recursive Call Downwards;
-        mark_curr_island(grid,x,y+1,r,c); // Makes Recursive Call Rightwards;
-        mark_curr_island(grid,x-1,y,r,c); // Makes Recursive Call to top;
-        mark_curr_island(grid,x,y-1,r,c); // Makes Recursive Call to left;
-
-    }
-    
-    int numIslands(vector<vector<char>>& grid) {
-       int rows = grid.size();
-
-        if(rows == 0)
-            return 0;
-
-        int columns = grid[0].size();
-
-        int numberOfIslands = 0;
-
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<columns;j++){
-                if(grid[i][j] == '1'){
-
-                    mark_curr_island(grid,i,j,rows,columns);
-                    numberOfIslands ++;
-
+   
+     void bfs(int row, int col, vector<vector<int>> &visited, vector<vector<char>> &grid){
+        int m = grid.size();
+        int n = grid[0].size();
+        visited[row][col] = 1;
+        queue<pair<int,int>> que;
+        que.push({row, col});
+        while(!que.empty()){
+            int row = que.front().first;
+            int col = que.front().second;
+            que.pop();
+            
+            int dx[4] = {-1, 0, 1, 0};
+            int dy[4] = {0, 1, 0, -1};
+            for(int i=0; i<4; i++){
+                    int nrow = row+dx[i];
+                    int ncol = col +dy[i];
+                    if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && !visited[nrow][ncol] && grid[nrow][ncol] == '1'){
+                        que.push({nrow, ncol});
+                        visited[nrow][ncol]=1;
+                    }
                 }
             }
         }
-        return numberOfIslands; 
+    
+  
+
+    /*void bfs(int row,int col,vector<vector<int>>&vis,vector<vector<char>>& grid)
+    {
+        vis[row][col]=1;
+        queue<pair<int,int>>q;
+        q.push({row,col});
+        int n=grid.size();
+        int m=grid[0].size();
+        while(!q.empty()){
+            int row=q.front().first;
+            int col=q.front().second;
+            q.pop();
+            int dr[4]={-1,0,1,0};
+           int dc[4]={0,-1,0,1};
+            for(int k=0;k<4;k++){
+               
+                    int newrow=row+dr[k];
+                    int newcol=col+dc[k];
+                    
+                    if(newrow>=0 && newrow<n && newcol>=0 && newcol<m && !vis[newrow][newcol] &&
+                          grid[newrow][newcol]=='1'){
+                        q.push({newrow,newcol});
+                       vis[newrow][newcol]=1;
+                        
+                    
+                }
+            }
+        }
+    }*/
+    
+    int numIslands(vector<vector<char>>& grid) {
+        
+       /* int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        int count=0;
+        
+        
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(!vis[i][j] && grid[i][j]=='1');
+                {
+                    count++;
+                    bfs(i,j,vis,grid);
+                }
+            }
+        }
+        return count;*/
+     int m = grid.size();
+        int n = grid[0].size();
+        int count=0;
+        vector<vector<int>> visited(m, vector<int>(n, 0));
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(!visited[i][j]  && grid[i][j] == '1') {
+                    count++;
+                    bfs(i, j, visited, grid);
+                }
+            }
+        }
+        return count;
     }
 };
 
