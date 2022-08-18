1531c9c4f61251b20e5fc1847072d8c262d13933
RakhiChaudhary <93258616+RakhiChaudhary@users.noreply.github.com>
2022-08-18 11:05:18 +0530

    Time: 111 ms (7.90%), Space: 19.2 MB (9.54%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,33 +1,9 @@
 class Solution {
 public:
    
-     void bfs(int row, int col, vector<vector<int>> &visited, vector<vector<char>> &grid){
-        int m = grid.size();
-        int n = grid[0].size();
-        visited[row][col] = 1;
-        queue<pair<int,int>> que;
-        que.push({row, col});
-        while(!que.empty()){
-            int row = que.front().first;
-            int col = que.front().second;
-            que.pop();
-            
-            int dx[4] = {-1, 0, 1, 0};
-            int dy[4] = {0, 1, 0, -1};
-            for(int i=0; i<4; i++){
-                    int nrow = row+dx[i];
-                    int ncol = col +dy[i];
-                    if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && !visited[nrow][ncol] && grid[nrow][ncol] == '1'){
-                        que.push({nrow, ncol});
-                        visited[nrow][ncol]=1;
-                    }
-                }
-            }
-        }
-    
-  
+   
 
-    /*void bfs(int row,int col,vector<vector<int>>&vis,vector<vector<char>>& grid)
+    void bfs(int row,int col,vector<vector<int>>&vis,vector<vector<char>>& grid)
     {
         vis[row][col]=1;
         queue<pair<int,int>>q;
@@ -54,11 +30,11 @@ public:
                 }
             }
         }
-    }*/
+    }
     
     int numIslands(vector<vector<char>>& grid) {
         
-       /* int n=grid.size();
+        int n=grid.size();
         int m=grid[0].size();
         vector<vector<int>>vis(n,vector<int>(m,0));
         int count=0;
@@ -67,15 +43,15 @@ public:
         
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(!vis[i][j] && grid[i][j]=='1');
+                if(!vis[i][j] && grid[i][j]=='1')
                 {
                     count++;
                     bfs(i,j,vis,grid);
                 }
             }
         }
-        return count;*/
-     int m = grid.size();
+        return count;
+/*     int m = grid.size();
         int n = grid[0].size();
         int count=0;
         vector<vector<int>> visited(m, vector<int>(n, 0));
@@ -87,7 +63,7 @@ public:
                 }
             }
         }
-        return count;
+        return count;*/
     }
 };
 
